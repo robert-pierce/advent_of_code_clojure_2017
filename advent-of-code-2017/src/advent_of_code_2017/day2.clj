95e89ebd44ac0898596f959789a47d4cfb8d0020
Robert Pierce <robert.pierce@careerbuilder.com>
2017-12-08 09:39:34 -0500

    "finished day 2"

--- advent-of-code-2017/src/advent_of_code_2017/day2.clj
+++ advent-of-code-2017/src/advent_of_code_2017/day2.clj
@@ -11,12 +11,30 @@
         minimum (apply min parsedRow)]
   (- maximum minimum)))
 
+(defn findRowDivsum
+  [row]
+  (let [parsedRow (map #(Integer/parseInt %) row)
+        rawResult (for [x parsedRow y parsedRow]
+                    (if (and  (not (= x y)) (= (mod x y ) 0))
+                      (/ x y)))]
+    (first (filter #(not (nil? %)) rawResult))))
+
 
 (defn day-2-part-1
-  "blah blah"
+  "Finds the sum of the differences between the 
+  maximum and minimum in each row"
   [body]
   (let [strBody (slurp body)
         splitBody (str/split strBody #"\n")
         splitRows  (map #(str/split % #"\t") splitBody)
         rowChecksums (map findRowChecksum splitRows)]
     {:status 200 :body (str (reduce + rowChecksums))}))
+
+(defn day-2-part-2
+  [body]
+  (let [strBody (slurp body)
+        splitBody (str/split strBody #"\n")
+        splitRows (map #(str/split % #"\t") splitBody)
+        rowDivsums (map findRowDivsum splitRows)]
+    (print splitBody)
+    {:status 200 :body (str (reduce + rowDivsums))}))
