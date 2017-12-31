5d951bf431c7ad399a668e6147db82539f57539a
Robert Pierce <robert.pierce@careerbuilder.com>
2017-12-31 14:11:19 -0500

    freaking finally finished the tree, better examples would have made this much easier. Example was a special case of the tree.

--- advent-of-code-2017/src/advent_of_code_2017/day7.clj
+++ advent-of-code-2017/src/advent_of_code_2017/day7.clj
@@ -40,99 +40,115 @@
         {active-node-name node-weight :child-weights child-node-weights}))))
 
 (defn extract-leaves
-"ADDED A FLATTEN"
-  [weight-map parent]
+  [tree weight-map parent]
   (let [node-name  (first (keys weight-map))
         node-weight (val (first weight-map))
         children (:child-weights weight-map)]
     (if (nil? children)
       (do 
-        (assoc weight-map :parent parent))
+        (assoc weight-map :parent parent :parent-weight (:node-weight (get tree parent))))
       (do
-        (flatten (map #(extract-leaves % node-name) children))))))
+        (map #(extract-leaves tree % node-name) children)))))
+
+(defn indices-of [f coll]
+  (keep-indexed #(if (f %2) %1 nil) coll))
+
+(defn first-index-of [f coll]
+  (first (indices-of f coll)))
+
+(defn find-thing [value coll]
+  (first-index-of #(= % value) coll))
+
+(defn get-different-node
+  [frequencies]
+  (first (->> frequencies
+              (group-by val)
+              (#(get % 1))
+              (map key))))
+
+(defn get-leaves-difference
+  [values leaf-names]
+  (let [freq (frequencies values)
+        distinct-values (distinct values)
+        different-node (get-different-node freq)
+        index (find-thing different-node values)
+        different-node-name (nth (vec leaf-names) index)
+        difference (first (remove #(= % 0)
+                                  (map #(- % different-node) distinct-values)))]
+    {:difference difference :leaf-name different-node-name}))
+
+(defn process-branch
+  [coll branch]
+  (let [name (key branch)
+        parent-weight (:parent-weight (val branch))
+        leaf-names (:leaf-names (val branch))
+        values (:updated-weight (val branch))
+        balanced? (apply = values)]
+    (if balanced? 
+      (assoc coll name {:leaf-names leaf-names :updated-weight (reduce + values) :parent-weight parent-weight })
+      (assoc coll name {:leaf-names leaf-names :updated-weight {:different (get-leaves-difference values leaf-names)} :parent-weight parent-weight} ))))
 
 (defn analyze-leaves-reducer
   [coll leaf]
   (let [parent (:parent leaf)
+        parent-weight (:parent-weight leaf)
+        leaf-name (key (first leaf))
         value (val (first leaf))
-        current-weights (get coll parent)
+        current-leaves (:leaf-names (get coll parent))
+        updated-leaves (conj current-leaves leaf-name)
+        current-weights (:updated-weight (get coll parent))
         updated-weights (conj current-weights value)]
-    (assoc coll parent updated-weights)))
-
-(defn get-leaves-difference
-  [values]
-  (let [freq (frequencies values)
-        distinct-values (distinct values)
-        different-node (first (->> freq
-                               (group-by val)
-                               (#(get % 1))
-                               (map key)))
-        difference (first (remove #(= % 0) 
-                                  (map #(- % different-node) distinct-values)))]
-    difference))
-
-(defn process-branch
-  "NEED TO WORK ON THIS Replacing analyze-leave, processing 
-  aggregated leaves from analyze-leaves-2. Need to do "
-  [coll branch]
-  (let [name (key branch)
-        values (val branch)
-        balanced? (apply = values)
-        ]
-    (if balanced? 
-      (assoc coll name (reduce + values))
-      (assoc coll name {:different (get-leaves-difference values)}))))
+    (assoc coll parent {:leaf-names updated-leaves :updated-weight  updated-weights :parent-weight parent-weight})))
 
 (defn analyze-leaves
   [leaves]
   (let [aggregated-leaves (reduce analyze-leaves-reducer {} leaves)]
-    (reduce process-branch {} aggregated-leaves)))
+     (reduce process-branch {} aggregated-leaves)))
 
-
-(defn remove-leaves-reducer
-  [tree children]
-  (apply #(dissoc tree %) children))
-
-(defn get-children-to-remove
-  [tree analyzed-leaves]
-  (let [parent-keys-to-collapse (map :parent analyzed-leaves)
-        nodes-to-collapse-on (map #(get tree %) parent-keys-to-collapse)
-        children (map :children nodes-to-collapse-on)]
-    (flatten children)))
 
 (defn collapse-tree-reducer
   [tree analyzed-leaf]
-  (let [parent-key-to-collapse (:parent analyzed-leaf)
-        sum-of-children (:sum analyzed-leaf)
+  (let [parent-key-to-collapse (key analyzed-leaf)
+        sum-of-children (:updated-weight (val analyzed-leaf))
         node-to-collapse-on (get tree parent-key-to-collapse)
         current-node-weight (:node-weight node-to-collapse-on)
-        node-to-collapse-children (:children node-to-collapse-on)
+        children-to-collapse (:leaf-names (val analyzed-leaf))
+        node-to-collapse-children (get node-to-collapse-on :children)
+        trimmed-children (remove (set children-to-collapse) node-to-collapse-children)
         collapsed-node (assoc node-to-collapse-on 
                          :node-weight (+ sum-of-children current-node-weight)
-                         :children nil)]
-    (assoc tree parent-key-to-collapse collapsed-node)))
+                         :children (if (= (count trimmed-children) 0) nil trimmed-children))
+        tree-with-trimmed-leaves (reduce #(dissoc %1 %2) tree children-to-collapse)]
+    (assoc tree-with-trimmed-leaves parent-key-to-collapse collapsed-node)))
+
+(defn extract-difference-reducer
+  [coll leaf]
+  (let [parent-name (key leaf)
+        leaf-val (val leaf)
+        updated-weight (:updated-weight leaf-val)
+        parent-weight (:parent-weight leaf-val)
+        is-map? (map? updated-weight)]
+    (if is-map? 
+      (assoc coll :answer 
+             {:node parent-name 
+              :node-weight parent-weight 
+              :difference updated-weight})
+      coll)))
 
 (defn process-move
-"FIGURE OUT AT ANALYZE LEAVES"
   [root analysis-map]
   (let [old-weight-map (:weight-map analysis-map)
         old-tree (:tree analysis-map)
-        extracted-leaves  (extract-leaves old-weight-map "root")
-        analyzed-leaves (map analyze-leaves extracted-leaves)
-        
-        difference (for [entry analyzed-leaves
-                         :let [difference (:difference entry)]
-                         :when (not (:balanced entry))]
-                     difference)]
+        extracted-leaves  (flatten (extract-leaves old-tree old-weight-map "root"))
+        analyzed-leaves (analyze-leaves extracted-leaves)
+        difference (reduce extract-difference-reducer {} analyzed-leaves)]
     (if (not (empty? difference))
-      (assoc analysis-map :difference (first difference))
+      (do
+        (assoc analysis-map :difference difference))
       (do
         (let [collapsed-tree (reduce collapse-tree-reducer old-tree analyzed-leaves)
-              children-to-remove (get-children-to-remove old-tree analyzed-leaves)
-              collapsed-tree-with-leaves-removed (apply dissoc collapsed-tree children-to-remove)
-              updated-weight-map (build-weight-map root collapsed-tree-with-leaves-removed)]
-          {:tree collapsed-tree-with-leaves-removed :weight-map updated-weight-map :difference nil}
-          )))))
+              updated-weight-map (build-weight-map root collapsed-tree)]
+          {:tree collapsed-tree :weight-map updated-weight-map :difference nil})))))
 
 (defn process-unbalanced-tree
   [initial-parsed-tree initial-weight-map root]
@@ -142,6 +158,16 @@
     (if (not (nil? (:difference analysis-map)))
       (:difference analysis-map)
       (recur (process-move root analysis-map)))))
+
+(defn process-tree
+  [initial-parsed-tree intial-weight-map root]
+  (let [answer (process-unbalanced-tree initial-parsed-tree intial-weight-map root)
+        culprit-leaf  (get-in answer [:answer :difference :different :leaf-name])
+        culprit-leaf-original-value (get-in initial-parsed-tree [culprit-leaf :node-weight])
+        culprit-leaf-difference (get-in answer [:answer :difference :different :difference])
+        final-leaf-value (+ culprit-leaf-difference culprit-leaf-original-value)
+        final-answer {:node-name culprit-leaf :new-balanced-value final-leaf-value}]
+   final-answer))
 
 
 (defn day-7-part-1
@@ -159,7 +185,7 @@
         split-on-rows (str/split str-body #"\n")
         split-on-arrows (map #(str/split % #"->") split-on-rows)
         parsed-tree (reduce parse-tree-reducer {} split-on-arrows)
-        root (find-root parsed-tree)
-        weight-map (build-weight-map (key (first root)) parsed-tree)
-         ]
-    {:status 200 :body (str "The answer is " weight-map )}))
+        root (key (first (find-root parsed-tree)))
+        weight-map (build-weight-map root parsed-tree)
+        answer  (process-tree parsed-tree weight-map root)]
+    {:status 200 :body (str "The answer is " answer )}))
